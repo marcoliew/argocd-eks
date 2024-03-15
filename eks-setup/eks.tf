75f8468c8f486d705cedb3f94b4aed1e585b6cd0
Marco Liao <marco.liao@sydney.edu.au>
2024-03-15 17:05:45 +1100

    eof

--- eks-setup/eks.tf
+++ eks-setup/eks.tf
@@ -1,20 +1,20 @@
 resource "aws_iam_role" "eks-role" {
   name = "${var.name_prefix}eks-cluster-role"
 
-#   assume_role_policy = <<POLICY
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": {
-#         "Service": "eks.amazonaws.com"
-#       },
-#       "Action": "sts:AssumeRole"
-#     }
-#   ]
-# }
-# POLICY
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
 }
 
 # resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
