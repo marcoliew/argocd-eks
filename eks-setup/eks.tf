7eebf9348e02e9a6225c08cdd9e6dc47ed710bf8
Marco Liao <marco.liao@sydney.edu.au>
2024-03-15 17:49:33 +1100

    3

--- eks-setup/eks.tf
+++ eks-setup/eks.tf
@@ -41,20 +41,21 @@ resource "aws_iam_role" "nodes-role" {
 EOF
 }
 
-# resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-#   role       = aws_iam_role.nodes-role.name
-# }
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.nodes-role.name
+}
 
-# resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-#   role       = aws_iam_role.nodes-role.name
-# }
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes-role.name
+}
 
-# resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-#   role       = aws_iam_role.nodes-role.name
-# }
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes-role.name
+}
+
 # resource "aws_eks_cluster" "argocdprj_eks" {
 #   name     = var.cluster-name
 #   role_arn = aws_iam_role.eks-role.arn
