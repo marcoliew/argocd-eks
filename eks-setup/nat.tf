f22fd7e7ff583e68d3c45be377585100f4de1fad
Marco Liao <marco.liao@sydney.edu.au>
2024-03-15 13:46:19 +1100

    eip

--- eks-setup/nat.tf
+++ eks-setup/nat.tf
@@ -1,13 +1,13 @@
-# resource "aws_eip" "nat" {
-#   vpc = true
+resource "aws_eip" "nat" {
+  vpc = true
 
-#   tags = {
-#     Name = "nat"
-#   }
-# }
+  tags = {
+    Name = "nat"
+  }
+}
 
 resource "aws_nat_gateway" "nat" {
-  #allocation_id = aws_eip.nat.id
+  allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public-ap-southeast-2a.id
 
   tags = {
