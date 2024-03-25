a7e8c5f7210ed2ac9236e0ed689c5ea09cdc0925
Marco Liao <marco.liao@sydney.edu.au>
2024-03-25 12:04:09 +1100

    typo3

--- eks-setup/route-table.tf
+++ eks-setup/route-table.tf
@@ -1,23 +1,21 @@
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route = [
-    {
-      cidr_block                 = "0.0.0.0/0"
-      nat_gateway_id             = aws_nat_gateway.nat.id
-      carrier_gateway_id         = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
-      gateway_id                 = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      vpc_peering_connection_id  = ""
-    }
-  ]
+  route = {
+    cidr_block                 = "0.0.0.0/0"
+    nat_gateway_id             = aws_nat_gateway.nat.id
+    carrier_gateway_id         = ""
+    destination_prefix_list_id = ""
+    egress_only_gateway_id     = ""
+    gateway_id                 = ""
+    instance_id                = ""
+    ipv6_cidr_block            = ""
+    local_gateway_id           = ""
+    network_interface_id       = ""
+    transit_gateway_id         = ""
+    vpc_endpoint_id            = ""
+    vpc_peering_connection_id  = ""
+  }
 
   tags = {
     Name = "${var.name_prefix}private-rt"
@@ -27,23 +25,21 @@ resource "aws_route_table" "private" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route = [
-    {
-      cidr_block                 = "0.0.0.0/0"
-      gateway_id                 = aws_internet_gateway.igw.id
-      nat_gateway_id             = ""
-      carrier_gateway_id         = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      vpc_peering_connection_id  = ""
-    }
-  ]
+  route = {
+    cidr_block                 = "0.0.0.0/0"
+    gateway_id                 = aws_internet_gateway.igw.id
+    nat_gateway_id             = ""
+    carrier_gateway_id         = ""
+    destination_prefix_list_id = ""
+    egress_only_gateway_id     = ""
+    instance_id                = ""
+    ipv6_cidr_block            = ""
+    local_gateway_id           = ""
+    network_interface_id       = ""
+    transit_gateway_id         = ""
+    vpc_endpoint_id            = ""
+    vpc_peering_connection_id  = ""
+  }
 
   tags = {
     Name = "${var.name_prefix}public-rt"
