dbc8483ffe0b69781c70953a923c68484298f5b6
Marco Liao <marco.liao@sydney.edu.au>
2024-03-25 12:26:28 +1100

    typo5

--- eks-setup/route-table.tf
+++ eks-setup/route-table.tf
@@ -4,6 +4,17 @@ resource "aws_route_table" "private" {
   route = [{
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
+    carrier_gateway_id         = ""
+    destination_prefix_list_id = ""
+    egress_only_gateway_id     = ""
+    gateway_id                 = ""
+    instance_id                = ""
+    ipv6_cidr_block            = ""
+    local_gateway_id           = ""
+    network_interface_id       = ""
+    transit_gateway_id         = ""
+    vpc_endpoint_id            = ""
+    vpc_peering_connection_id  = ""
   }]
 
   tags = {
@@ -17,6 +28,17 @@ resource "aws_route_table" "public" {
   route = [{
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
+    nat_gateway_id             = ""
+    carrier_gateway_id         = ""
+    destination_prefix_list_id = ""
+    egress_only_gateway_id     = ""
+    instance_id                = ""
+    ipv6_cidr_block            = ""
+    local_gateway_id           = ""
+    network_interface_id       = ""
+    transit_gateway_id         = ""
+    vpc_endpoint_id            = ""
+    vpc_peering_connection_id  = ""
   }]
 
   tags = {
