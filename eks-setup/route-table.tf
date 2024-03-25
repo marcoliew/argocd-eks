61aa0f043f3264526dafed4f1e516f215abb42ef
Marco Liao <marco.liao@sydney.edu.au>
2024-03-25 12:22:32 +1100

    typo5

--- eks-setup/route-table.tf
+++ eks-setup/route-table.tf
@@ -1,10 +1,10 @@
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route = {
+  route = [{
     cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
-  }
+  }]
 
   tags = {
     Name = "${var.name_prefix}private-rt"
@@ -14,10 +14,10 @@ resource "aws_route_table" "private" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route = {
+  route = [{
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
-  }
+  }]
 
   tags = {
     Name = "${var.name_prefix}public-rt"
