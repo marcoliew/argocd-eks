44f06b7a8637b096bb69bc1a54cf61a8934a8192
Marco Liao <marco.liao@sydney.edu.au>
2024-03-25 12:38:05 +1100

    final

--- eks-setup/route-table.tf
+++ eks-setup/route-table.tf
@@ -1,22 +1,10 @@
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route = [{
+  route {
     cidr_block                 = "0.0.0.0/0"
     nat_gateway_id             = aws_nat_gateway.nat.id
-    carrier_gateway_id         = ""
-    destination_prefix_list_id = ""
-    egress_only_gateway_id     = ""
-    gateway_id                 = ""
-    instance_id                = ""
-    ipv6_cidr_block            = ""
-    local_gateway_id           = ""
-    network_interface_id       = ""
-    transit_gateway_id         = ""
-    vpc_endpoint_id            = ""
-    vpc_peering_connection_id  = ""
-    core_network_arn           = ""
-  }]
+  }
 
   tags = {
     Name = "${var.name_prefix}private-rt"
@@ -26,22 +14,10 @@ resource "aws_route_table" "private" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route = [{
+  route {
     cidr_block                 = "0.0.0.0/0"
     gateway_id                 = aws_internet_gateway.igw.id
-    nat_gateway_id             = ""
-    carrier_gateway_id         = ""
-    destination_prefix_list_id = ""
-    egress_only_gateway_id     = ""
-    instance_id                = ""
-    ipv6_cidr_block            = ""
-    local_gateway_id           = ""
-    network_interface_id       = ""
-    transit_gateway_id         = ""
-    vpc_endpoint_id            = ""
-    vpc_peering_connection_id  = ""
-    core_network_arn           = ""
-  }]
+  }
 
   tags = {
     Name = "${var.name_prefix}public-rt"
